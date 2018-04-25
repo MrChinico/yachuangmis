18015991bd7b85199de3d3144d99670c724ca826
wangxiaoqing <xiaoqingwang@126.com>
2018-04-26 00:02:18 +0800

    编辑病人表单

--- pcapp/src/components/patientinfo/edit.js
+++ pcapp/src/components/patientinfo/edit.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Layout,Button } from 'antd';
 import lodashget from 'lodash.get';
-
-
+import EditpatientinfoForm from './form_editpatientinfo';
+const { Header } = Layout;
 class App extends React.Component {
 
 
@@ -14,6 +14,9 @@ class App extends React.Component {
 		componentWillUnmount() {
 
 		}
+		onClickSubmit =(values)=>{
+			this.props.history.goBack();
+		}
 
   	render() {
 			const {curpaientinfo} = this.props;
@@ -21,22 +24,25 @@ class App extends React.Component {
 				return <div>无病人信息</div>
 			}
 	    return (
-	      	<Layout>
-						<div>头部标题栏</div>
-						<span><Button onClick={
+				<Layout>
+				<Header>
+					<span><img src="index.png" className="icon-index" alt=""/>病人列表-张三丰详情信息</span>
+				</Header>
+				<div className="content-box">
+					<div className="content">
+						<h2>21206<span>张三丰</span><button className="return" onClick={
 							()=>{
 								this.props.history.goBack();
 							}
-						}>返回上页</Button></span>
-						<div>修改表单</div>
-						<div>
-							<Button onClick={
-								()=>{
-									this.props.history.goBack();
-								}
-							}>保存更改</Button>
+						}><img src="return.png" alt=""/></button>
+						<div className="clearfix"></div>
+						</h2>
+
+						<EditpatientinfoForm curpaientinfo={curpaientinfo}  onClickSubmit={this.onClickSubmit}/>
+
 						</div>
-	      	</Layout>
+				</div>
+				</Layout>
 	    );
   	}
 }
