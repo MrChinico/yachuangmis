b9796f08744a9d8f9bb617193db574e49aae311c
wangxiaoqing <xiaoqingwang@126.com>
2018-05-06 18:37:23 +0800

    view&

--- pcapp/src/components/evaluate/recordwoundsurface.js
+++ pcapp/src/components/evaluate/recordwoundsurface.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ViewPrintHeader from './viewprint_header';
 import lodashget from 'lodash.get';
-import RecordwoundsurfaceTablebodyTitle from './recordwoundsurface_tablebody_title';
+import RecordwoundsurfaceTablebody from './recordwoundsurface_tablebody';
 
 import { Layout } from 'antd';
 const { Header } = Layout;
@@ -18,7 +18,7 @@ class App extends React.Component {
 		}
 
   	render() {
-			const {curpaientinfo,db} = this.props;
+			const {curpaientinfo,db,evaluatewoundsurfacelist} = this.props;
 			if(!curpaientinfo){
 				return <div>无病人信息</div>
 			}
@@ -41,164 +41,11 @@ class App extends React.Component {
 							<div className="clearfix"></div>
 						</h1>
 						<form>
-							<RecordwoundsurfaceTablebodyTitle />
 							<div className="form-box">
 								<h1>某某市某某医院危险因素评估</h1>
 								<ViewPrintHeader curpaientinfo={curpaientinfo} db={db} />
-
-								<table width="100%" className="nursing-record white-bg">
-									<tr>
-										<td colspan="" rowspan="2">创面</td>
-										<td colspan="" rowspan="2">创面大小</td>
-										<td colspan="" rowspan="2">项目指标</td>
-										<td colspan="4"><div align="center">评估日期</div></td>
-									</tr>
-									<tr className="date">
-										<td><div align="center"><img src="arrow-left-green.png" />4月11日</div></td>
-										<td><div align="center">4月10日</div></td>
-										<td><div align="center">4月9日</div></td>
-										<td><div align="center">4月8日<img src="arrow-right-green.png" className="right" /></div></td>
-									</tr>
-									<tr>
-										<td rowspan="9" className="white-bg">创面一</td>
-										<td colspan="2" align="center">分期</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td colspan="2" align="center">部位</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td rowspan="7">创面大小</td>
-										<td align="center">长cm</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td align="center">宽cm</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td align="center">深cm</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td align="center">潜行</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td align="center">窦道</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td align="center">颜色</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td align="center">渗液量</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td rowspan="9" className="white-bg">创面二</td>
-										<td colspan="2" align="center">分期</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td colspan="2" align="center">部位</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td rowspan="7">创面大小</td>
-										<td align="center">长cm</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td align="center">宽cm</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td align="center">深cm</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td align="center">潜行</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td align="center">窦道</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td align="center">颜色</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td align="center">渗液量</td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-										<td align="center"></td>
-									</tr>
-									<tr>
-										<td colspan="3" className="white-bg">评估人
-										<input type="text" />
-										</td>
-										<td align="center" className="white-bg"></td>
-										<td align="center" className="white-bg"></td>
-										<td align="center" className="white-bg"></td>
-										<td align="center" className="white-bg"></td>
-									</tr>
-									</table>
+								<RecordwoundsurfaceTablebody evaluatewoundsurfacelist={evaluatewoundsurfacelist} db={db} />
+								
 								</div>
 						</form>
 					</div>
@@ -209,10 +56,11 @@ class App extends React.Component {
 }
 
 
-const mapStateToProps = ({db},props) => {
+const mapStateToProps = ({db,evaluatewoundsurface},props) => {
 		const {paientinfos} = db;
 		const id = lodashget(props,'match.params.pid');
+		const {evaluatewoundsurfacelist} = evaluatewoundsurface;
 		let curpaientinfo = paientinfos[id];
-    return {curpaientinfo,db};
+    return {curpaientinfo,db,evaluatewoundsurfacelist};
 }
 export default connect(mapStateToProps)(App);
---
+++ pcapp/src/components/evaluate/recordwoundsurface_tablebody.js
@@ -0,0 +1,250 @@
+import React from 'react';
+import lodashmap from 'lodash.map';
+import lodashget from 'lodash.get';
+
+import moment from 'moment';
+import {getpagelist} from '../../util';
+
+class RecordbardenTableBody extends React.Component {
+
+      constructor(props) {
+        super(props);
+        const {isfirst,islast,retlist} = getpagelist(0,props.evaluatewoundsurfacelist,4);
+        this.state = {
+          current: 0,
+          isfirst,
+          islast,
+          retlist
+        };
+      }
+      PageNext = ()=>{
+        const current = this.state.current + 1;
+        const {isfirst,islast,retlist} = getpagelist(current,this.props.evaluatewoundsurfacelist,4);
+        this.setState({
+          current,
+          isfirst,
+          islast,
+          retlist
+        });
+      }
+      PagePrev = ()=>{
+        const current = this.state.current - 1;
+        const {isfirst,islast,retlist} = getpagelist(current,this.props.evaluatewoundsurfacelist,4);
+        this.setState({
+          current,
+          isfirst,
+          islast,
+          retlist
+        });
+      }
+  		componentDidMount(){
+
+  		}
+
+  		componentWillUnmount() {
+
+  		}
+      render() {
+            const {evaluatewoundsurfaces,users} = this.props.db;
+            const {isfirst,islast,retlist} = this.state;
+            let tabletrs = [];
+
+            tabletrs.push(
+              <tr>
+                <td rowspan="9" className="white-bg">创面一</td>
+                <td colspan="2" align="center">分期</td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+              </tr>);
+
+            tabletrs.push(
+              <tr>
+                <td colspan="2" align="center">部位</td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+              </tr>);
+
+
+              tabletrs.push(
+              <tr>
+                <td rowspan="7">创面大小</td>
+                <td align="center">长cm</td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+              </tr>);
+
+              tabletrs.push(
+              <tr>
+                <td align="center">宽cm</td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+              </tr>);
+
+              tabletrs.push(
+              <tr>
+                <td align="center">深cm</td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+              </tr>);
+
+              tabletrs.push(
+              <tr>
+                <td align="center">潜行</td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+              </tr>);
+
+              tabletrs.push(
+              <tr>
+                <td align="center">窦道</td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+              </tr>);
+
+              tabletrs.push(
+              <tr>
+                <td align="center">颜色</td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+              </tr>);
+
+            tabletrs.push(
+              <tr>
+                <td align="center">渗液量</td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+                <td align="center"></td>
+              </tr>
+            );
+            return (<table width="100%" className="nursing-record white-bg">
+                <tbody>
+                <tr>
+                  <td colspan="" rowspan="2">创面</td>
+                  <td colspan="" rowspan="2">创面大小</td>
+                  <td colspan="" rowspan="2">项目指标</td>
+                  <td colspan="4"><div align="center">评估日期</div></td>
+                </tr>
+                <tr className="date">
+                  {
+                    lodashmap(retlist,(info,index)=>{
+                      const curday = moment(info.updated_at).format('MM月DD日')
+                      if(index === 0){
+                        return (<td key={index}><div align="center">{ !isfirst && <img src="arrow-left-green.png" alt="" onClick={
+                                this.PagePrev
+                              }/>}{curday}</div></td>);
+                      }
+                      else if(index === retlist.length - 1){
+                          return (<td  key={index}><div align="center">{curday}
+                            {!islast && <img src="arrow-right-green.png" className="right" alt="" onClick={
+                              this.PageNext
+                            }/>}
+                          </div></td>);
+                      }
+                      else{
+                        return (<td  key={index}><div align="center">{curday}</div></td>);
+                      }
+
+                    })
+                  }
+                </tr>
+
+                {tabletrs}
+
+                <tr>
+                  <td rowspan="9" className="white-bg">创面二</td>
+                  <td colspan="2" align="center">分期</td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                </tr>
+                <tr>
+                  <td colspan="2" align="center">部位</td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                </tr>
+                <tr>
+                  <td rowspan="7">创面大小</td>
+                  <td align="center">长cm</td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                </tr>
+                <tr>
+                  <td align="center">宽cm</td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                </tr>
+                <tr>
+                  <td align="center">深cm</td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                </tr>
+                <tr>
+                  <td align="center">潜行</td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                </tr>
+                <tr>
+                  <td align="center">窦道</td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                </tr>
+                <tr>
+                  <td align="center">颜色</td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                </tr>
+                <tr>
+                  <td align="center">渗液量</td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                  <td align="center"></td>
+                </tr>
+                <tr>
+                  <td colspan="3" className="white-bg">评估人
+                  <input type="text" />
+                  </td>
+                  <td align="center" className="white-bg"></td>
+                  <td align="center" className="white-bg"></td>
+                  <td align="center" className="white-bg"></td>
+                  <td align="center" className="white-bg"></td>
+                </tr>
+                </tbody>
+                </table>);
+            }
+
+}
+
+export default RecordbardenTableBody;
