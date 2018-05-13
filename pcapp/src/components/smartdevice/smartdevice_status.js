597322387edec64d19be1f7f4ac718650647ca20
wangxiaoqing <xiaoqingwang@126.com>
2018-05-13 22:34:24 +0800

    修正样式问题

--- pcapp/src/components/smartdevice/smartdevice_status.js
+++ pcapp/src/components/smartdevice/smartdevice_status.js
@@ -4,15 +4,18 @@ import lodashget from 'lodash.get';
 const SmartDeviceStatus = (props)=>{
   const {cursmartdevice} = props;
   console.log(cursmartdevice);
-  return (
-    <div className="device-info">
-    <span className="device-name">智能床ID:{lodashget(cursmartdevice,'deviceid','')}</span>
-    <span>设备状态：<font>{lodashget(cursmartdevice,'realtimedata.establishstatus','')}</font></span>
-    <span>气垫方位：<font>{lodashget(cursmartdevice,'realtimedata.positionstring','')}</font></span>
-    <span>气垫角度：<font>{lodashget(cursmartdevice,'realtimedata.anglestring','')}</font></span>
+  return [
+      <div className="device-info" key="1">
+      <span className="device-name">智能床ID:{lodashget(cursmartdevice,'deviceid','')}</span>
+      <span>设备状态：<font>{lodashget(cursmartdevice,'realtimedata.establishstatus','')}</font></span>
+      <span>气垫方位：<font>{lodashget(cursmartdevice,'realtimedata.positionstring','')}</font></span>
+      <span>气垫角度：<font>{lodashget(cursmartdevice,'realtimedata.anglestring','')}</font></span>
+    </div>,
+    <div className="device-info" key="2" style={{marginBottom: '30px'}}>
     <span>当前姿态建立状态：<font>{lodashget(cursmartdevice,'realtimedata.establishstatusstring','')}</font></span>
     <span>最后上报时间：<font className="warn-color">{lodashget(cursmartdevice,'realtimedata.lastupdatetime','')}</font></span>
-  </div>);
+    </div>
+  ];
 }
 
 export default SmartDeviceStatus;
